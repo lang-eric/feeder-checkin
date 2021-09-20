d10054d3a89eac6c8e6da9d62c703556592d8064
Eric Lang <Eric@mail.rit.edu>
2021-09-20 02:06:15 -0400

    created admin page, but it doesn't work... why

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var adminRouter = require('./routes/admin');
 
 var app = express();
 
@@ -18,7 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/admin', adminRouter);
+app.use('/po', indexRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/admin.js
+++ routes/admin.js
@@ -1,16 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+// TODO DRY.
 const JSONdb = require('simple-json-db');
 const db = new JSONdb('database.json');
 
 
-/* GET home page. */
-router.get('/po', function (req, res, next) {
+/* GET admin page. */
+router.get('/admin', function (req, res, next) {
     res.render('index', {
         title: 'RIT Cats - Feeder Check-In.', feeders: db.get('feeders')
     });
 });
+
 
 
 module.exports = router;
