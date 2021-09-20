583be44082716b496d6911cb5a61e70a92891be8
Eric Lang <Eric@mail.rit.edu>
2021-09-20 01:45:00 -0400

    implemented a database!

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+const JSONdb = require('simple-json-db');
+const db = new JSONdb('database.json');
+
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
     res.render('index', {
-        title: 'RIT Cats - Feeder Check-In.', feeders: ['Eric L', 'Emily L', 'Lily G']
+        title: 'RIT Cats - Feeder Check-In.', feeders: db.get('feeders')
     });
 });
 
